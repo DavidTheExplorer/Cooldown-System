cfd07a613025fcfc980b54ae677bce3af0a06872
David <darknight4123@gmail.com>
2023-08-05 17:16:09 +0300

    Simplified ChronoUnitUtils

--- src/main/java/dte/cooldownsystem/utils/ChronoUnitUtils.java
+++ src/main/java/dte/cooldownsystem/utils/ChronoUnitUtils.java
@@ -1,27 +1,18 @@
 package dte.cooldownsystem.utils;
 
 import java.time.temporal.ChronoUnit;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.commons.lang.WordUtils;
 
 public class ChronoUnitUtils 
 {
-	private static final Map<ChronoUnit, String> SINGULAR_NAMES = new HashMap<>();
-	
 	public static String getCorrectName(ChronoUnit unit, long unitAmount) 
 	{
 		return WordUtils.capitalizeFully(unitAmount > 1 ? unit.name() : getSingularName(unit));
 	}
 	
-	public static String getSingularName(ChronoUnit unit) 
-	{
-		return SINGULAR_NAMES.computeIfAbsent(unit, ChronoUnitUtils::createSingularName);
-	}
-	
 	//removes the last character (e.g SECONDS -> SECOND)
-	private static String createSingularName(ChronoUnit unit) 
+	private static String getSingularName(ChronoUnit unit) 
 	{
 		return unit.name().substring(0, unit.name().length()-1);
 	}
