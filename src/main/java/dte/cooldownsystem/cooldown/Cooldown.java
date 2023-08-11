a7e79fa1668899983999f368f43ec77a71421d58
David <darknight4123@gmail.com>
2023-08-11 21:37:35 +0300

    Cooldown#getPlayersUUIDs internally now works similarly to #toMap

--- src/main/java/dte/cooldownsystem/cooldown/Cooldown.java
+++ src/main/java/dte/cooldownsystem/cooldown/Cooldown.java
@@ -1,11 +1,10 @@
 package dte.cooldownsystem.cooldown;
 
-import static java.util.stream.Collectors.toSet;
-
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -249,9 +248,9 @@ public class Cooldown
 	 */
 	public Set<UUID> getPlayersUUIDs()
 	{
-		return this.endDates.keySet().stream()
-				.filter(this::isOnCooldown)
-				.collect(toSet());
+		refresh();
+		
+		return new HashSet<>(this.endDates.keySet());
 	}
 
 	/**
